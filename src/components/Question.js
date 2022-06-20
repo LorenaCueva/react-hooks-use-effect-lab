1e7bf96439152377857af0b4e3169953d2bc6db6
Lorena Cueva <lorenacueva@gmail.com>
2022-06-20 14:49:07 -0700

    commit tree d35fcdef1f2085ed8a2d72e4edfdf3b29f3bebc5

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      timeRemaining > 0 ?
+       setTimeRemaining((timeRemaining) => timeRemaining - 1) 
+       : handleAnswer(false);
+    },1000)
+
+    return function cleanup(){
+      clearTimeout(timeoutID);
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
